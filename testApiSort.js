20b25c876a1d0f78873cd1b04881491e2223bf53
underethsea <underethsea@gmail.com>
2022-07-13 21:22:59 +0000

    go

--- testApiSort.js
+++ testApiSort.js
@@ -1,5 +1,5 @@
 const fetch = require('cross-fetch')
-const drawId = 250
+const drawId = 271
 const prizeCap = 1
 const chains = [{
     name: "Avalanche",
@@ -23,102 +23,105 @@ const chains = [{
 }
 ]
 // module.exports = async (drawId) => {
-    async function test(drawId) {
-    
+async function test(drawId) {
+
+    // initialize variables
     let totalPrizeLength = 0
-    let totalAmounts = 0
+    let prizeNetworkClaimableCount = 0
+    let prizeNetworkClaimable = 0
+    let prizeNetworkDropped = 0
     let prizesAllChains = []
+
     // loop through all of the supported chains
     for (let chain of chains) {
         const api = await fetchApi(drawId, chain)
-        if(api !== undefined) {
-        let consolidated = consolidateByAddress(api)
-        console.log(consolidated)
-        // const prizeJson = api.apiPrizeJson
-        // const statusJson = api.apiStatusJson
-        // console.log(statusJson)
-        // const prizeLength = statusJson.meta.prizeLength // number of prizes
-        // const amountsTotal = statusJson.meta.amountsTotal // total prizes for chain in 6 decimal for usdc
-        const prizeLength = api.length
-        let amountsTotal = 0;
-        api.forEach(prize=>{amountsTotal += parseFloat(prize.amount)})
-        totalPrizeLength += prizeLength
-        totalAmounts += parseFloat(amountsTotal) / 1e6;
+        // check api returned
+        if (api !== undefined) {
+            // group results by address
+            let consolidated = await consolidateByAddress(api)
+            let sortedConsolidated = []
+            // loop through results by address
+            for (let winner in consolidated) {
+                let prizes = consolidated[winner].amount.sort().reverse()
+                let prizesFloat = []
 
-        let prizesAddedChain = []
+                // yuckkky yuckerson
+                prizes.forEach(p => {
+                    let float = parseFloat(p)
+                    float = float / 1e6
+                    float = float.toFixed(0)
+                    float = float.toString()
+                    prizesFloat.push(float)
+                })
 
-        // add chain id to each prize entry
-        api.forEach(win => {
-            let prizeWin = win
-            prizeWin.chain = chain.chainId;
-            prizesAddedChain.push(prizeWin)
-        })
-        // put chains draw results together into one array
-        prizesAllChains = [...prizesAllChains, ...prizesAddedChain]
-        // console.log("Total Prizes for Draw ", drawId, " - ", totalPrizeLength)
-        // console.log("Total Prize value for Draw ", drawId, " - ", totalAmounts)
-        // console.log("Total prizes counted",prizesAllChains.length)
-        
+                // split claimable prizes from dropped according to prize cap
+                let claimable = prizesFloat.slice(0, prizeCap)
+                let dropped = prizesFloat.slice(prizeCap, prizes.length)
 
-        
-    }}
-    await checkMismatchPrizes(prizesAllChains,drawId)
-        let prizeApiStatus = {
-            totalPrizeCt: prizesAllChains.length,
-            totalPrizeAmt: totalAmounts.toFixed(0)
+                // sum claimable and dropped arrays
+                let totalClaimable = claimable.reduce((partialSum, a) => partialSum + (parseFloat(a)), 0);
+                let totalDropped = dropped.reduce((partialSum, a) => partialSum + (parseFloat(a)), 0);
+
+                // add to total prize count for the draw
+                totalPrizeLength += parseFloat(api.length);
+
+                // total amount and value of prizes claimable and dropped on this specific network
+                prizeNetworkClaimableCount += claimable.length
+                prizeNetworkClaimable += totalClaimable;
+                prizeNetworkDropped += totalDropped;
+
+                // add address to chain prize array
+                sortedConsolidated.push({
+                    n: chain.chainId,
+                    a: consolidated[winner].address,
+                    k: consolidated[winner].pick,
+                    c: claimable,
+                    u: dropped,
+                    w: totalClaimable,
+                    d: totalDropped
+                })
+            }
+
+            // add chain to mega prize return
+            prizesAllChains = [...prizesAllChains, ...sortedConsolidated]
+
         }
-        return prizeApiStatus;
-   
     }
 
-    async function fetchApi(drawId, chain) {
-        //        console.log("Fetching prize API data for ",chain.chainId)
-        // create api urls based on chain and draw information
-        // const statusApiUrl = "https://api.pooltogether.com/prizes/" + chain.chainId + "/" + chain.prizeDistributor + "/draw/" + drawId + "/status.json";
-        const prizesApiUrl = "https://api.pooltogether.com/prizes/" + chain.chainId + "/" + chain.prizeDistributor + "/draw/" + drawId + "/prizes.json";
-        try {
-            // fetch api status for specified draw and chain
-            // const apiStatusReturn = await fetch(statusApiUrl)
-            // const apiStatusJson = await apiStatusReturn.json()
-            const prizeApiReturn = await fetch(prizesApiUrl)
-            const prizeApiJson = await prizeApiReturn.json()
-            return prizeApiJson
+    console.log(prizesAllChains)
+    console.log("total prizes: ", totalPrizeLength)
+    console.log("total prizes claimable ", prizeNetworkClaimableCount)
+    console.log("total claimable: ", prizeNetworkClaimable)
+    console.log("total dropped: ", prizeNetworkDropped)
+    return prizesAllChains
 
-        } catch (error) { console.log("api fetch fail for draw ",drawId," chain ",chain.chainId) }
+}
+
+async function fetchApi(drawId, chain) {
+    console.log("Fetching prize API data for ", chain.chainId)
+
+    // api urls based on chain and draw information
+    // const statusApiUrl = "https://api.pooltogether.com/prizes/" + chain.chainId + "/" + chain.prizeDistributor + "/draw/" + drawId + "/status.json";
+    const prizesApiUrl = "https://api.pooltogether.com/prizes/" + chain.chainId + "/" + chain.prizeDistributor + "/draw/" + drawId + "/prizes.json";
+    try {
+        // fetch api status for specified draw and chain
+        // const apiStatusReturn = await fetch(statusApiUrl)
+        // const apiStatusJson = await apiStatusReturn.json()
+        const prizeApiReturn = await fetch(prizesApiUrl)
+        const prizeApiJson = await prizeApiReturn.json()
+        return prizeApiJson
+
+    } catch (error) { console.log("api fetch fail for draw ", drawId, " chain ", chain.chainId) }
+}
+
+async function consolidateByAddress(results) {
+
+    const consolidatedResult = {};
+    for (const { address, pick, amount } of results) {
+        if (!consolidatedResult[address]) consolidatedResult[address] = { address, pick, amount: [] };
+        consolidatedResult[address].amount.push(amount);
     }
-
-    async function consolidateByAddress(results) {
-     //   var cars = [{ make: 'audi', model: 'r8', year: '2012' }, { make: 'audi', model: 'rs5', year: '2013' }, { make: 'ford', model: 'mustang', year: '2012' }, { make: 'ford', model: 'fusion', year: '2015' }, { make: 'kia', model: 'optima', year: '2012' }],
-    consolidatedResult = results.reduce(function (r, a) {
-        r[a.address] = r[a.address] || [];
-        r[a.address].push(a.amount)
-        return r;
-    }, Object.create(null));
     return consolidatedResult;
 }
 
-
-    async function checkMismatchPrizes(prizesAllChains,drawId){
-    //check for unmatched prizes
-//	console.log(prizesAllChains.length,"   prizes for draw ",drawId);
-    let explorerApi = await fetch("https://poolexplorer.xyz/draw" + drawId)
-    explorerApi = await explorerApi.json()
-    explorerApi.forEach(player => {
-        if (player.w > 0) {
-            let explorerFilter = explorerApi.filter(function (address) { return address.a === player.a })
-            let playerPrizes = prizesAllChains.filter(function (address) { return address.address === player.a })
-            let prizesForPlayer = 0
-            let explorerPrizesForPlayer = 0
-            explorerFilter.forEach(prizesAccrossNetworks => {
-                explorerPrizesForPlayer += prizesAccrossNetworks.c.length + prizesAccrossNetworks.u.length
-            })
-            playerPrizes.forEach(win => {
-                prizesForPlayer += 1;
-            })
-            if (prizesForPlayer !== explorerPrizesForPlayer) {
-                console.log("draw ",drawId,player.a, " prize API prizes  ", prizesForPlayer, " poolexplorer prizes ", explorerPrizesForPlayer)
-            }
-        }
-    })
-}
 test(drawId)
